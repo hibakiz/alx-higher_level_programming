8df6ecb7cf3d1ca89dd2e47a2c604597d8428cfe
hibakiz <hbosalah98sd@gmail.com>
2023-11-01 00:41:12 +0300

    nnnnnnnnnnoooooooooooooo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 i = 0 
 j = 1
-
-    
-print(", ".join(["{}{}".format(min(i, j), max(i, j))  for j in range(i + 1, 10) for i in range(9) if i != j]))
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i != j:
+            print(", ".join(["{}{}".format(i, j)]))
