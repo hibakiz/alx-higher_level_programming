094627b044593821ba147419d26f502bec00711a
hibakiz <hbosalah98sd@gmail.com>
2023-11-01 00:54:16 +0300

    nnnnnnnnnnoooooooooooooo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-i = 0 
-j = 1
-for i in range(9):
-    for j in range(i + 1, 10):
-        if i != j:
-            print(", ".join(["{}{}".format(i, j)]))
+for i in range(0, 9):
+     for j in range(i+1, 10):
+         if i == 8 and j == 9:
+             print('89')
+         else:
+             print("{}{}, ".format(i, j), end="")
