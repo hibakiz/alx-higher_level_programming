beb19c457aa1f69b6c9f111c44ff9a35d0bd8583
hibakiz <hbosalah98sd@gmail.com>
2023-11-02 23:31:12 +0300

    nnnnnnnnnnoooooooooooooo

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ a = 10
 b = 5
 sum = 0
 if __name__ == "__main__":
-	from sys import argv
-	for i in range(1, len(argv)):
-		sum += int(argv[i])
-	print("{:d}".format(sum))
+    from sys import argv
+    for i in range(1, len(argv)):
+        sum += int(argv[i])
+    print("{:d}".format(sum))
