0e3ab21f36a9dfe9f5fa2b027702f699d1ef6cb2
hibakiz <hbosalah98sd@gmail.com>
2023-11-02 22:34:17 +0300

    nnnnnnnnnnoooooooooooooo

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-c = add(a, b)
-print("{} + {} = {}".format(a, b, c))
+if __name__ == "__add__":
+    from add_0 import add
+    a = 1
+    b = 2
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
