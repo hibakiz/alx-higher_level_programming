cbaa5a1b71e5235695a774ba78e1b8bf8391c955
hibakiz <hbosalah98sd@gmail.com>
2023-11-05 00:07:48 +0300

    4th day task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string and len(my_string) > 0:
-        return "".join([i for i in my_string if i != 'c' and i != 'C'])
+    if my_string:
+        new = ''
+        for i in my_string:
+            if i != 'c' and i != 'C':
+                new += i
+    return (new)
