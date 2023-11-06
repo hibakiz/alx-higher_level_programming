cb8a0b3a6d231406e585f2deff4e717e00a1cc8a
hibakiz <hbosalah98sd@gmail.com>
2023-11-06 22:22:34 +0300

    4th day task 2

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -6,6 +6,5 @@ tuple_b = (88, 11)
 new_tuple = add_tuple(tuple_a, tuple_b)
 print(new_tuple)
 
-print(add_tuple(tuple_a, (1, 2, 3)))
-
-print(add_tuple((), (1,4)))
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
