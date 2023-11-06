f2c66698e422b114339f23588e1bec8e490e87c4
hibakiz <hbosalah98sd@gmail.com>
2023-11-06 23:47:52 +0300

    4th day task 2

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,71 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - rsdfghujikt
+ * @head: pdfghjnklt
+ * Return: pdrftghujikkot
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+}
+
+/**
+ * is_palindrome - edrftgyhujikoome
+ * @head: dowertyu7i90opt
+ *
+ * Return: rtyu8i9o0tyutgifyguit
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	while (1)
+	{
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
+	}
+
+	reverse_listint(&dup);
+
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!dup)
+		return (1);
+
+	return (0);
+}
