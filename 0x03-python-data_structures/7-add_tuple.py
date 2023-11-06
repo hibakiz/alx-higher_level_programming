2ad4e97c8565594b2d2222313b8ac501db2a4eca
hibakiz <hbosalah98sd@gmail.com>
2023-11-06 22:25:12 +0300

    4th day task 2

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,11 +3,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
     a = list(tuple_a)
     b = list(tuple_b)
     sum = []
-    for i in range(0, 2):
-        if i >= len(b):
-            b.append(0)
-        elif i >= len(a):
-            a.append(0)
-        sum.append(a[i] + b[i])
-    new = tuple(sum)
-    return (new)
+    if a or b:
+        for i in range(0, 2):
+            if i >= len(b):
+                b.append(0)
+            elif i >= len(a):
+                a.append(0)
+            sum.append(a[i] + b[i])
+        new = tuple(sum)
+        return (new)
