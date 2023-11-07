dc17afc21b1782213367c4b82b9299d5b667560a
hibakiz <hbosalah98sd@gmail.com>
2023-11-07 22:06:57 +0300

    rev for lists

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,11 +7,18 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *prev, *node, *new;
 
-	if (!head)
+	if (head == NULL || *head == NULL)
 		return(NULL);
 	node = *head;
 	prev = node;
 	node = node->next;
+	if (node->n > number)
+	{
+		new = malloc(sizeof(listint_t));
+		new->n = number;
+		new->next = node;
+		return(new);
+	}
 	while (node)
 	{
 		if (node->n > number)
