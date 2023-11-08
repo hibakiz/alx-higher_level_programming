c6151666f62d064566b8871c06adbe629abb261f
hibakiz <hbosalah98sd@gmail.com>
2023-11-09 00:13:22 +0300

    more and more of more more and more

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+    if (a_dictionary.get(key)):
+        a_dictionary[key] = value
+    else:
+        a_dictionary.setdefault(key, value)
+    return (a_dictionary)
