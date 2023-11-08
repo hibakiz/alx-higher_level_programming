aa3f5d494ce098f5885a50b23b707697a1103a2b
hibakiz <hbosalah98sd@gmail.com>
2023-11-08 23:42:55 +0300

    more and more of more more and more

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
+    od = 1
     if set_1 or set_2:
         list1 = list(set_1)
         list2 = list(set_2)
         od_list = []
+        first_list = []
+        sec_list = []
         for i in list1:
-            for j in list2:
-                if i == j:
-                    list1.remove(i)
-                    list2.remove(j)
-        od_list = list1 + list2
+            if od:
+                for j in list2:
+                    if i == j:
+                        od = 0
+                        break
+                if od:
+                    first_list.append(i)
+                od = 1
+        for i in list2:
+            if od:
+                for j in list1:
+                    if i == j:
+                        od = 0
+                        break
+                if od:
+                    sec_list.append(i)
+                od = 1
+        od_list = first_list + sec_list
         return ((od_list))
     return ([])
--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 only_diff_elements = __import__('4-only_diff_elements').only_diff_elements
 
-set_1 = {}
-set_2 = {}
+set_1 = { "Python", "C", "Javascript", "Perl", "99"}
+set_2 = { "Bash", "C", "Ruby", "Perl", "99"}
 od_set = only_diff_elements(set_1, set_2)
 print(sorted(list(od_set)))
