542217e6c2202d57988272f39e4035b61d1a5a85
hibakiz <hbosalah98sd@gmail.com>
2023-11-08 22:50:39 +0300

    more and more of more more and more

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    list1 = list(set_1)
-    list2 = list(set_2)
-    od_list = []
-    for i in list1:
-        for j in list2:
-            if i == j:
-                list1.remove(i)
-                list2.remove(j)
-    od_list = list1 + list2
-    return ((od_list))
+    if set_1 or set_2:
+        list1 = list(set_1)
+        list2 = list(set_2)
+        od_list = []
+        for i in list1:
+            for j in list2:
+                if i == j:
+                    list1.remove(i)
+                    list2.remove(j)
+        od_list = list1 + list2
+        return ((od_list))
+    return ([])
