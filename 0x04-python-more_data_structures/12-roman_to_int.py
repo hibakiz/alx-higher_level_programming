a78d9b5bed20be679231e2f67437dbe2fae82ac3
hibakiz <hbosalah98sd@gmail.com>
2023-11-09 01:57:51 +0300

    more and more of more more and more

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    summ = 0
     if isinstance(roman_string, str):
         units = {'I': 1, 'V': 5, 'X': 10}
         units.update({'L': 50, 'C': 100,  'D': 500, 'M': 100})
-        summ = 0
         strr = ''.join(reversed(roman_string))
         for i in range(0, len(strr)):
             if units.get(strr[i]):
