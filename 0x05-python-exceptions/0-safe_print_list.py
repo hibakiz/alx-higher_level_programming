6791304d86cc61ee7c7eb274d732dd0df72688b7
hibakiz <hbosalah98sd@gmail.com>
2023-11-21 01:48:16 +0300

    I can't stop putting ;

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     idx = 0
-    if x <= 0 or not my_list:
-        return (0)
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end='')
