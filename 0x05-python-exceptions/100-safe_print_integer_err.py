526cf7ae7b7308d5b0b7774c7bcbf6a73859dc6d
hibakiz <hbosalah98sd@gmail.com>
2023-11-21 03:15:30 +0300

    I can't stop putting ;

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,11 +2,33 @@
 def safe_print_integer_err(value):
     import sys
     import traceback
-
+    first = "Exception: Unknown format code 'd' for object of type "
     try:
         print("{:d}".format(value))
         return (True)
     except ValueError as e:
-        msg = "Exception: Unknown format code 'd' for object of type 'str'"
+        if isinstance(value, int):
+            dtype = 'int'
+        elif isinstance(value, float):
+            dtype = "float"
+        elif isinstance(value, complex):
+            dtype = "complex"
+        elif isinstance(value, str):
+            dtype = "str"
+        elif isinstance(value, bytes):
+            dtype = "bytes"
+        elif isinstance(value, bytearray):
+            dtype = "bytearray"
+        elif isinstance(value, list):
+            dtype = "list"
+        elif isinstance(value, tuple):
+            dtype = "tuple"
+        elif isinstance(value, set):
+            dtype = "set"
+        elif isinstance(value, dict):
+            dtype = "dict"
+        else:
+            dtype = "unknown data type"
+        msg = first + "'{}'".format(dtype)
         print(msg, file=sys.stderr)
         return (False)
