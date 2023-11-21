3b126bc7311954852e0c4efa7ff5efddd294675c
hibakiz <hbosalah98sd@gmail.com>
2023-11-21 18:24:44 +0300

    I hate OOP

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' The sqqure module'''
+
+
+class Square:
+    '''define a squre
+
+    Args:
+        sq : the square '''
+    sq = {}
+
+    def __init__(self, size):
+        self.__size = size
