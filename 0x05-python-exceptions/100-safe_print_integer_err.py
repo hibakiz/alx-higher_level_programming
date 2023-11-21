5fbb680c3ad25607db1c7bd35d4bb68183b2616d
hibakiz <hbosalah98sd@gmail.com>
2023-11-21 03:17:58 +0300

    I can't stop putting ;

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except ValueError as e:
+    except (ValueError, TypeError) :
         if isinstance(value, float):
             dtype = "float"
         elif isinstance(value, complex):
