b4948f85cb69b41fde9cafccde780e9d8249e126
hibakiz <hbosalah98sd@gmail.com>
2023-11-21 20:17:27 +0300

    I hate OOP

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -34,6 +34,11 @@ class Square:
             value: the size.
         """
 
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
