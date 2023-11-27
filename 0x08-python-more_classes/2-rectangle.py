6fc1ab5a34227ab8cee83dc9af4d87d25c4513f8
hibakiz <hbosalah98sd@gmail.com>
2023-11-27 21:14:27 +0300

    I hate OOP

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+''' This is a Rectangle module'''
+
+
+class Rectangle:
+    ''' this is the Rectangle class'''
+    def __init__(self, width=0, height=0):
+        ''' Rectangle attrub
+        Args:
+            width: the width is the width of the rec
+            height: the height is the height of the rec'''
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        '''Getter'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''Setter
+        Args:
+            value: to add the width'''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        '''Getter'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''Setter
+        Args:
+            value: to add the height'''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.height + self.width)
