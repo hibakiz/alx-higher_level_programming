a93bc99c87a6b806bbda4afe86bdc491a802c99e
hibakiz <hbosalah98sd@gmail.com>
2023-11-28 20:33:12 +0300

    TDD is harikaaaaaaaaa

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Adds two integers.
+    Args:
+        a (int): The first integer to add.
+        b (int): The second integer to add.
+    Returns: int: The sum of the two integers.'''
+
+
+def add_integer(a, b=98):
+    '''Adds two integers.
+    a + b
+    '''
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    if a == 'inf' or a == '-inf':
+       raise OverflowError("OverflowError: cannot convert float infinity to integer")
+    if b == 'inf' or b == '-inf':
+       raise OverflowError("OverflowError: cannot convert float infinity to integer")
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
