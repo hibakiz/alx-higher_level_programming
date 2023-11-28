11d73543d3a114c9ecbf7a68271953e206237b43
hibakiz <hbosalah98sd@gmail.com>
2023-11-28 20:13:07 +0300

    Day 3 of I hate OOP

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+''' Locked class moduel'''
+
+
+class LockedClass:
+    ''' the LockedClass to prevent adding attrubites rather than first name'''
+    __slots__ = ['first_name'] 
