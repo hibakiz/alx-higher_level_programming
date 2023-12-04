269dedf5fd6e7d7f55e4afd5152f6d1ad26b4c7a
hibakiz <hbosalah98sd@gmail.com>
2023-12-04 23:40:47 +0300

    I think I will pes etmek

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" Rectangle module"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """BaseGeometry Rec class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.width = width
+        self.integer_validator("height", height)
+        self.height = height
