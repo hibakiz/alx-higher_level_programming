0cef7d3bc843ba3c8c8ad632430ddd71734fce11
hibakiz <hbosalah98sd@gmail.com>
2023-12-04 22:05:40 +0300

    I think I will pes etmek

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" is kind of a class module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ the function to cpmare
+    Args: obj and a_class
+    """
+    return (isinstance(obj, a_class))
