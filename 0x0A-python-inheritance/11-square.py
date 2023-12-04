dcae166ac7f893b1fde5d48cb1cb2eefafbaab6e
hibakiz <hbosalah98sd@gmail.com>
2023-12-04 23:51:19 +0300

    I think I will pes etmek

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" Sq module"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """sq calss"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
