9e79889bd48007bafbb175558620492ceaacd111
hibakiz <hbosalah98sd@gmail.com>
2023-12-04 22:13:56 +0300

    I think I will pes etmek

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" inhirites from or not"""
+
+
+def inherits_from(obj, a_class):
+    """ the function to check
+    Args: obj and a_class
+    """
+    return (issubclass(type(obj), a_class) and type(obj) is not a_class)
