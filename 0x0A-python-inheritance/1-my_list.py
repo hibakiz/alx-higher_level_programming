7a66479acf525ae636076f92ccd00d7833beefbc
hibakiz <hbosalah98sd@gmail.com>
2023-12-04 21:37:27 +0300

    I think I will pes etmek

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+MyList module
+"""
+
+
+class MyList(list):
+    """ print a sorted list
+    no Args no Return
+    """
+
+    def print_sorted(self):
+        """print soreted"""
+        print(sorted(self))
