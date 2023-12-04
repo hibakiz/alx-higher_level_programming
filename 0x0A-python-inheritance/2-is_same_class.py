6058152e19bec7d0f423bc18a35c5b0bc8af98e6
hibakiz <hbosalah98sd@gmail.com>
2023-12-04 22:03:29 +0300

    I think I will pes etmek

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+""" Test the type"""
+
+
+def is_same_class(obj, a_class):
+    """return true or flase for comparing the obj withthe data type"""
+    return (type(obj) is a_class)
