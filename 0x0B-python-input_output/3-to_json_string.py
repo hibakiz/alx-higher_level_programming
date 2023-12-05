a7e70d26d623b7a111259802570ce63dadbca778
hibakiz <hbosalah98sd@gmail.com>
2023-12-05 20:42:08 +0300

    Ajaza istiyorum

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" write to file module with json"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """ write to file function with json
+    Args:
+    my_obj: the object to convert
+    """
+    return json.dumps(my_obj)
