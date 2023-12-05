fd7ca3c99592e006eb09d4e50e4fc2ec51bea517
hibakiz <hbosalah98sd@gmail.com>
2023-12-05 20:31:00 +0300

    Ajaza istiyorum

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" write to file module"""
+
+
+def append_write(filename="", text=""):
+    """ write to file function
+    Args:
+    filename: the file name
+    text: the text to write to the file
+    """
+    with open(filename, 'a') as f:
+        x = f.write(text)
+        return (x)
