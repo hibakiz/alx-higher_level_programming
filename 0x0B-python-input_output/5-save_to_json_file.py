b79d76ad176ec8dbfdb7b2a6522bb88cd1f2a062
hibakiz <hbosalah98sd@gmail.com>
2023-12-05 21:02:17 +0300

    Ajaza istiyorum

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" write to a file using json"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ write to a file using json
+    Args:
+    my_obj: the object to convert to json
+    filename: the file to write to
+    """
+    with open(filename,'w') as f:
+        json.dump(my_obj, f)
