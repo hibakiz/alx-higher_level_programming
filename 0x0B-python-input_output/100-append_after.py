befe37ba6137fc209f48a200e3a1a9aa801921cc
hibakiz <hbosalah98sd@gmail.com>
2023-12-05 22:52:01 +0300

    Ajaza istiyorum

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""append after module"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """append after function
+    Args:
+        filename
+        search_string
+        new_string
+    """
+    text = ""
+    with open(filename) as f:
+        for line in f:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
