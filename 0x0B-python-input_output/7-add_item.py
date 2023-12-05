a11bfcf3cf1af2c23b461af45774d75a088f4cde
hibakiz <hbosalah98sd@gmail.com>
2023-12-05 21:24:39 +0300

    Ajaza istiyorum

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" load from a file using json"""
+
+import json
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        listt = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        listt = []
+    listt.extend(sys.argv[1:])
+    save_to_json_file(listt, "add_item.json")
