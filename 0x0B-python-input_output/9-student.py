f930353da4cdb67d7fb9105c2cace39cbf0eac3f
hibakiz <hbosalah98sd@gmail.com>
2023-12-05 21:42:44 +0300

    Ajaza istiyorum

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """Class student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Init new Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """get the dictof json"""
+        return self.__dict__
