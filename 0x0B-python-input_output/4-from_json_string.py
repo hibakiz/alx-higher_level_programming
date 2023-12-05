d220bfc735962588e4e49a93783fdb20c76db6f9
hibakiz <hbosalah98sd@gmail.com>
2023-12-05 20:53:18 +0300

    Ajaza istiyorum

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" decode from json"""
+
+import json
+
+
+def from_json_string(my_str):
+    """ decode from  json
+    Args:
+    my_str: the object to convert from json
+    """ 
+    return json.loads(my_str)
