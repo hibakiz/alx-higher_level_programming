8598a38efd1c4ea59317acc96df5075489c3a472
hibakiz <hbosalah98sd@gmail.com>
2023-12-05 20:29:00 +0300

    Ajaza istiyorum

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" write to file module"""
+
+
+def write_file(filename="", text=""):
+    """ write to file function
+    Args:
+    filename: the file name
+    text: the text to write to the file
+    """
+    with open(filename, 'a') as f:
+        x = f.write(text)
+        return (x)
