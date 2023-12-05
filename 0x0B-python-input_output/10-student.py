e3bcfb2da3a155646a5414bbfbdc94f9c57bf86a
hibakiz <hbosalah98sd@gmail.com>
2023-12-05 21:45:29 +0300

    Ajaza istiyorum

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """Class student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Init new Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """get a dict of the Student
+        """
+        if (type(attrs) is list and
+                all(type(i) is str for i in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
