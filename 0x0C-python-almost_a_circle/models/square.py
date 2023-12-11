562c09484c013c692ab11d6cd19a4060bdce9fb3
hibakiz <hbosalah98sd@gmail.com>
2023-12-11 20:07:49 +0300

    bay checker lutfen iyi ol

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -64,3 +64,10 @@ class Square(Rectangle):
 					self.x = value
 				elif key == "y":
 					self.y = value
+	# to dictionary
+	def to_dictionary(self):
+		"""retrun the dictionary of a Rectangle"""
+		return {'id': self.id,
+		'size': self.size,
+		'x': self.x,
+		'y': self.y}	
