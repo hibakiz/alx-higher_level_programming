28c527066d5f1775c5d35c579852a90eb89b6ae8
hibakiz <hbosalah98sd@gmail.com>
2023-12-11 19:54:08 +0300

    bay checker lutfen iyi ol

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Suare module"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+	"""Square class"""
+	def __init__(self, size, x=0, y=0, id=None):
+		"""inint function"""
+		self.size = size
+		self.x = x
+		self.y = y
+		self.id = None
+		super().__init__(size, size, x, y, id)
+
+
+	@property
+	def size(self):
+		"""Size Getter"""
+		return self.__width
+
+	@size.setter
+	def size(self, size):
+		"""Size settrt"""
+		if type(size) is not int:
+			raise TypeError("width nust be an integer")
+		if size <= 0:
+			raise ValueError("width must be > 0")
+		self.__width = size
+		self.__height = size
+		
+
+	def __str__(self):
+		"""Update the str function"""
+		return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+	# The updtae function
+	def update(self, *args, **kwargs):
+		"""assigns an argument to each attribute"""
+		if args and len(args) != 0:
+			for i in range(len(args)):
+				if i == 0:
+					if i is None:
+						self.__init__(self.width, self.height, self.x, self.y)
+					else:
+						self.id = args[0]
+				elif i == 1:
+					self.width = args[1]
+				elif i == 2:
+					self.x = args[2]
+				elif i == 3:
+					self.y = args[3]
+		else:
+			for key, value in kwargs.items():
+				if key == "id":
+					if value is None:
+						self.__init__(self.width, self.height, self.x, self.y)
+					else:
+						self.id = value
+				elif key == "size":
+					self.width = value
+				elif key == "x":
+					self.x = value
+				elif key == "y":
+					self.y = value
