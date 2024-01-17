b86d128438defb75ddd6d25eec980c09c9d67ea6
hibakiz <hbosalah98sd@gmail.com>
2024-01-18 02:25:53 +0300

    kafam cok karasik

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 --  script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT id INTO @cal_id FROM states WHERE name = 'Calfornia';
+SELECT id INTO @cal_id FROM states WHERE name = 'California';
 SELECT id, name FROM cities WHERE state_id = @cal_id ORDER BY id;
