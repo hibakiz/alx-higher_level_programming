b09f252f623f5a0c61ed52d9369eedf5849bfbd9
hibakiz <hbosalah98sd@gmail.com>
2024-02-15 13:56:51 +0300

    ws

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+const listt = require('./100-data.js').listt;
+console.log(listt);
+console.log(listt.map((item, index) => item * index));
