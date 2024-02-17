bd46869ab717ae1b67ca875e48b31a6f9b3e6a44
hibakiz <hbosalah98sd@gmail.com>
2024-02-17 15:16:24 +0300

    090000000000

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     arg = list(sys.argv)
     db = MySQLdb.connect(user=arg[1], password=arg[2], database=arg[3])
     cursor = db.cursor()
-    cursor.("""SELECT * FROM states ORDER BY id ASC;""")
+    cursor.execute("""SELECT * FROM states ORDER BY id ASC;""")
     for row in cursor:
         print(row)
 
