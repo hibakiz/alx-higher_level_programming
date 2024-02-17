db08e813651a87cfaf4c19bb2c9608e7355b6aed
hibakiz <hbosalah98sd@gmail.com>
2024-02-17 15:25:20 +0300

    host and port

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,8 +11,10 @@ if __name__ == "__main__":
     lists all states from the database hbtn_0e_0_usa
     """
     arg = list(sys.argv)
-    db = MySQLdb.connect(user=arg[1], password=arg[2], database=arg[3])
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=arg[1], passwd=arg[2], db=arg[3])
     cursor = db.cursor()
     cursor.execute("""SELECT * FROM states""")
-    for row in cursor:
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
