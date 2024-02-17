77d04f25fca5a43bc8b527b29d105a89dfb5e816
hibakiz <hbosalah98sd@gmail.com>
2024-02-17 15:41:33 +0300

    port and so on

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,8 +11,9 @@ if __name__ == "__main__":
     lists all states from the database hbtn_0e_0_usa
     """
     db = MySQLdb.connect(
-        host="localhost", port=3306,
-        user=argv[1], password=argv[2],
+        host="localhost", user=argv[1],
+        port=3306,
+        password=argv[2],
         database=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states")
