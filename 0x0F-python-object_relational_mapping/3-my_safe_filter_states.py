0560bad9da62621eaffef02e7484d81895366e12
hibakiz <hbosalah98sd@gmail.com>
2024-02-18 20:02:27 +0300

    ORM

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+lists all states starts with N
+from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    lists all states from the database hbtn_0e_0_usa
+    """
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], database=argv[3])
+    cursor = db.cursor()
+    deli = ";"
+    state_name = argv[4].split(deli)
+    qury = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(state_name[0])
+    cursor.execute(qury)
+    for row in cursor:
+        print(row)
