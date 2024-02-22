734dc635196d279778d9b0dc7e075dd7eda4464b
hibakiz <hbosalah98sd@gmail.com>
2024-02-22 15:06:02 +0300

    ORM

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+lists all states starts with N
+from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """
+    lists all states from the database hbtn_0e_0_usa
+    """
+    username = argv[1]
+    password = argv[2]
+    db = argv[3]
+
+    engine = create_engine(f"mysql+mysqldb://{username}:\
+                            {password}@localhost:3306/{db}")
+    Base.metadata.bind = engine
+    dbsession = sessionmaker(bind=engine)
+    session = dbsession()
+    new = State(name="Louisiana")
+    session.add(new)
+    session.commit()
+
+    print(new.id)
