9ba95e1ab90a944fac01c99c971945c13858d888
hibakiz <hbosalah98sd@gmail.com>
2024-02-22 14:42:23 +0300

    ORM

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+lists all states starts with N
+from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """
+    lists all states from the database hbtn_0e_0_usa
+    """
+    username = argv[1]
+    password = argv[2]
+    db = argv[3]
+
+    engine = create_engine(f"mysql+mysqldb://{username}:\
+                            {password}@localhost:3306/{db}")
+    Base.metadata.bind = engine
+    dbsession = sessionmaker(bind=engine)
+    session = dbsession()
+    result = session.query(State).first()
+
+    if result:
+        print(f"{result.id}: {result.name}")
+    else:
+        print("Nothing")
