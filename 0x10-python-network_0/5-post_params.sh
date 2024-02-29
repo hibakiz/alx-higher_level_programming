c4e8e05dcc6ebc8b83165e678763ab75e9f71514
hibakiz <hbosalah98sd@gmail.com>
2024-02-29 19:47:10 +0300

    I love network without python ama

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-data="email=test@gmail.com&subject=I will always be here for PLD"
-curl -s -X POST -d "$data" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
