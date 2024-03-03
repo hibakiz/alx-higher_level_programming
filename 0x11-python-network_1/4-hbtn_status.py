f14dd17d8a4a220e764810196d7eb855dcf15620
hibakiz <hbosalah98sd@gmail.com>
2024-03-03 20:27:48 +0300

    Tired yaaaaaaaaa5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""script that fetches
+"""
+
+
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
+            
