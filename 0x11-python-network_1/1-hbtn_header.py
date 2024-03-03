b93d9cb4d6cc561ff96dad2531665c3583179e75
hibakiz <hbosalah98sd@gmail.com>
2024-03-03 19:37:34 +0300

    Tired yaaaaaaaaa5

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""script that fetches
+https://alx-intranet.hbtn.io/status
+"""
+
+
+import urllib.request
+from os import sys
+
+
+url = sys.argv[1]
+
+with urllib.request.urlopen(url) as page:
+    headers = dict(page.getheaders())
+    id = headers.get("X-Request-Id")
+    print(id)
+
