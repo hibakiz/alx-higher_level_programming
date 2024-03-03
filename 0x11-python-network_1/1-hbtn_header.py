792092bfe07e9bd9783aff23f22233e52b6e4a8e
hibakiz <hbosalah98sd@gmail.com>
2024-03-03 19:48:50 +0300

    Tired yaaaaaaaaa5

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,7 +9,6 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    request = urllib.request.Request(url)
     with urllib.request.urlopen(url) as page:
         headers = dict(page.getheaders())
         id = headers.get("X-Request-Id")
