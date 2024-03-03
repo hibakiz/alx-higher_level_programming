85c7b5b6ddf6e163ad2113b2a43e0f5e914775ef
hibakiz <hbosalah98sd@gmail.com>
2024-03-03 20:33:42 +0300

    Tired yaaaaaaaaa5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""script that fetches
+"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    if 'X-Request-Id' in response.headers:
+        print(response.headers['X-Request-Id'])
