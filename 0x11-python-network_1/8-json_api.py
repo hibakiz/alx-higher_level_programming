9642b48ee83cd30c272bf95b72d462a38b4b419c
hibakiz <hbosalah98sd@gmail.com>
2024-03-03 20:52:36 +0300

    Tired yaaaaaaaaa5

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""script that fetches
+"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    data = {
+            "q": q
+            }
+    response = requests.post(url, data=data)
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            if data:
+                print(f"[{data.get('id')}] {data.get('name')}")
+            else:
+                print("No result")
+        except ValueError:
+            print("Not a valid JSON")
