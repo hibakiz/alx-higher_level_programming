ee6447e56ee96b72a4b6597c00dddda03423f3f3
hibakiz <hbosalah98sd@gmail.com>
2024-03-03 19:56:52 +0300

    Tired yaaaaaaaaa5

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""script that fetches
+"""
+
+
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = f"email={sys.argv[2]}"
+    encoded = data.encode("utf-8")
+    request = urllib.request.Request(url, data=encoded, method="POST")
+    with urllib.request.urlopen(request) as page:
+        print(page.read().decode("utf-8"))
