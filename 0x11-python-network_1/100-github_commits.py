521b34cb6f176e62cb005ca33d244b0683256070
hibakiz <hbosalah98sd@gmail.com>
2024-03-03 21:18:28 +0300

    Tired yaaaaaaaaa5

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that fetches
+"""
+
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+
+    response = requests.get(url)
+    commits = response.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
