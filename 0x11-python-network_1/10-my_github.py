7421d6893dc3fb916da9b53559d3b3c01fc52735
hibakiz <hbosalah98sd@gmail.com>
2024-03-03 21:12:03 +0300

    Tired yaaaaaaaaa5

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""script that fetches
+"""
+
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    email = sys.argv[1]
+    password = sys.argv[2]
+    auth = HTTPBasicAuth(email, password)
+    response = requests.get(url, auth=auth)
+    data = response.json()
+    print(data.get("id"))
