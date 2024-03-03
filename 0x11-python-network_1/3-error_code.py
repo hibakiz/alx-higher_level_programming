5bfaf0fad06bc5da5f8c2c87b635dead06c25ed5
hibakiz <hbosalah98sd@gmail.com>
2024-03-03 20:05:04 +0300

    Tired yaaaaaaaaa5

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""script that fetches
+"""
+
+
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        request = urllib.request.Request(url)
+        with urllib.request.urlopen(request) as page:
+            print(page.read().decode("utf-8"))
+    except urllib.error.HTTPError as http_error:
+        print(f"Error code: {http_error.code}")
