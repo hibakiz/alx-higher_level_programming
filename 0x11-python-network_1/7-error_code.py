3dc4e5d57f4bed80a26d1448e2c32aa4cb6be698
hibakiz <hbosalah98sd@gmail.com>
2024-03-03 20:41:27 +0300

    Tired yaaaaaaaaa5

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""script that fetches
+"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
